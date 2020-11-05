a0e61d4eae29d56c888f0f1408a8d8439c850dad
yuki-snow1823 <magic.iroha@gmail.com>
2020-11-06 08:36:51 +0900

    add: 名前付きルートの設定

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', 'App\Http\Controllers\HelloController@index');
+Route::get('/hello', 'HelloController@index')->name('hello');
+Route::get('/hello/other', 'HelloController@other');
