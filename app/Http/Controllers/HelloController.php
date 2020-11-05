b28d80b4f0ac728dad6d43ed743a98348b8b830b
yuki-snow1823 <magic.iroha@gmail.com>
2020-11-06 08:31:37 +0900

    add: 起動までの復習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,10 +1,17 @@
 <?php
-
 namespace App\Http\Controllers;
+
 
 use Illuminate\Http\Request;
 
+
 class HelloController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            'msg'=>'this is sample message.',
+        ];
+        return view('hello.index', $data);
+    }
 }
