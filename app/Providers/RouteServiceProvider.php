32631325fd49a27cc46bd6f73f75c2cf82cffc33
yuki-snow1823 <magic.iroha@gmail.com>
2020-11-07 16:52:11 +0900

    fix: 修正

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use App\Models\Person;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -35,8 +36,20 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        parent::boot();
-        Route::model('person', Person::class);
+        $this->configureRateLimiting();
+        Route::model('person',Person::class);
+
+
+        $this->routes(function () {
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
+        });
     }
 
     /**
