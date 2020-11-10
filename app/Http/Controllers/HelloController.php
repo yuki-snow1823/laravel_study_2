0da7c80ca28c807aaab764c7099430a9527ddc20
yuki-snow1823 <magic.iroha@gmail.com>
2020-11-10 20:56:47 +0900

    あっd：Storageのpublicフォルダ参照

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,25 +11,25 @@ class HelloController extends Controller
 
     public function __construct()
     {
-        $this->fname = 'sample.txt';
+        $this->fname = 'hello.txt';
     }
 
 
     public function index()
     {
-        $sample_msg = $this->fname;
-        $sample_data = Storage::get($this->fname);
+        $sample_msg = Storage::disk('public')->url($this->fname);
+        $sample_data = Storage::disk('public')->get($this->fname);
         $data = [
             'msg'=> $sample_msg,
             'data'=> explode(PHP_EOL, $sample_data)
         ];
         return view('hello.index', $data);
     }
-
-
-public function other($msg)
-{
-    Storage::append($this->fname, $msg);
-    return redirect()->route('hello');
-}
+    
+    public function other($msg)
+    {
+        Storage::disk('public')->prepend($this->fname, $msg);
+        return redirect()->route('hello');
+    }    
+    
 }
