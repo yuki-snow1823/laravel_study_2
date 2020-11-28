5c5b81809e58e079e417cb84bbbdd25ae085f4a6
yuki-snow1823 <magic.iroha@gmail.com>
2020-11-28 12:16:42 +0900

    add

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,22 +2,21 @@
 namespace App\Http\Controllers;
 
 
-use App\MyClasses\MyService;
+use App\MyClasses\MyServiceInterface;        // 追加
 
 
 class HelloController extends Controller
 {
-    function __construct(MyService $myservice)
+    function __construct()
     {
-        $myservice = app('App\MyClasses\MyService');
     }
 
 
-    public function index(MyService $myservice, int $id = -1)
+    public function index(MyServiceInterface $myservice, int $id = -1)
     {
         $myservice->setId($id);
         $data = [
-            'msg'=> $myservice->say($id),
+            'msg'=> $myservice->say(),
             'data'=> $myservice->alldata()
         ];
         return view('hello.index', $data);
