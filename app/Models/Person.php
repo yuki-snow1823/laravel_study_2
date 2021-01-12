ae2d584aa9c4a7ca162fdc5e9f794a53549f722d
yuki-snow1823 <magic.iroha@gmail.com>
2021-01-12 20:10:00 +0900

    add: モデルメソッドの追加

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\Request;
 
 class Person extends Model
 {
     use HasFactory;
+
+    public function index(Request $request)
+{
+    $msg = 'show people record.';
+    $result = Person::get(); // 何をgetしてるの？全部？
+    $data = [
+        'msg' => $msg,
+        'data' => $result,
+    ];
+    return view('hello.index', $data);
+}
 }
